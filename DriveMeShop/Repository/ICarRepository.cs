24934f9bf47cb62ac10a22c1a06caf449fe6a0ce
Mamadou Diop <m.abdoulayediop@useradgents.com>
2021-03-18 18:07:25 +0100

    implement car repository

---
+++ DriveMeShop/Repository/ICarRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using DriveMeShop.Entity;
+
+namespace DriveMeShop.Repository
+{
+    public interface ICarRepository
+    {
+        public Task<string> CreateAsync(Car car);
+    }
+}
---
+++ DriveMeShop/Repository/implementation/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using DriveMeShop.Entity;
+using MongoDB.Driver;
+
+namespace DriveMeShop.Repository.implementation
+{
+    public class CarRepository: ICarRepository
+    {
+        private readonly IMongoCollection<Car> carCollection;
+
+        public CarRepository(IMongoCollection<Car> mongoCollection)
+        {
+            carCollection = mongoCollection;
+        }
+
+        public async System.Threading.Tasks.Task<string> CreateAsync(Car car)
+        {
+            await carCollection.InsertOneAsync(car);
+
+            return car.Id;
+        }
+    }
+}
