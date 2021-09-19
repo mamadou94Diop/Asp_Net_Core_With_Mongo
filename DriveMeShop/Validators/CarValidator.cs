c439eb128dee8efd7f56123c3ed2e02e6eaf8f3e
Mamadou Diop <m.abdoulayediop@useradgents.com>
2021-09-19 04:01:43 +0200

    add fluent validator

---
+++ DriveMeShop/Validators/CarValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DriveMeShop.Model;
+using FluentValidation;
+
+namespace DriveMeShop.Validators
+{
+    public class CarModelValidator: AbstractValidator<CarModel>
+    {
+
+        public CarModelValidator()
+        {
+            RuleFor(carModel => carModel)
+                .Must(IsReleasedYearBeforeLastRevisionYear)
+                .WithMessage("LastRevisionYear should not be before ReleasedYear");
+
+            RuleFor(carModel => carModel)
+                .Must(AreModelAndMakeDifferent)
+                .WithMessage("Make and Model should not be the same");
+        }
+
+        private bool AreModelAndMakeDifferent(CarModel carModel)
+        {
+            return carModel.Make.ToLower() != carModel.Model.ToLower();
+        }
+
+        private bool IsReleasedYearBeforeLastRevisionYear(CarModel carModel)
+        {
+            return carModel.ReleasedYear <= carModel.LastRevisionYear;
+        }
+    }
+}
