daf15f367aa11d7b5c1f51545833d9d24604dba2
Mamadou Diop <m.abdoulayediop@useradgents.com>
2021-09-19 04:01:25 +0200

    add some custom annotations

---
+++ DriveMeShop/CustomAnnotations/GreaterOrEqualThanAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DriveMeShop.CustomAnnotations
+{
+    public class GreaterOrEqualThanAttribute: ValidationAttribute
+    {
+        public int Minimum { get; set; }
+        public override bool IsValid(object value)
+        {
+            return (int)value >= Minimum;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return  $"Should be greater or equal to {Minimum}";
+        }
+    }
+}
---
+++ DriveMeShop/CustomAnnotations/YearStartingByAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DriveMeShop.CustomAnnotations
+{
+    public class YearStartingByAttribute : ValidationAttribute
+    {
+        public int StartingYear {get; set;}
+
+        private readonly int currentYear = DateTime.Now.Year;
+        
+
+        public override bool IsValid(object value)
+        {
+            var year = (int)value;
+            return year >= StartingYear && year <= currentYear;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return $"Year value should be between {StartingYear} and {currentYear}";
+        }
+    }
+}
