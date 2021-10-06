7a98d0be79846b914d38e7c97dd4713d74a0c13f
Mamadou Diop <m.abdoulayediop@useradgents.com>
2021-10-06 03:39:51 +0200

    write integration tests

---
+++ IntegrationTests/CarsControllerTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DriveMeShop.Model;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+    public class CarsControllerTests
+    {
+        private HttpClient httpClient;
+        private TestApplicationFactory testApplicationFactory;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            testApplicationFactory = new TestApplicationFactory();
+            httpClient = testApplicationFactory.CreateClient(); 
+        }
+
+        [Test]
+        public async Task given_a_car_when_insert_is_succesful_then_return_201_status()
+        {
+            //Arrange
+            var car = new CarModel
+            {
+                Make = "Toyota",
+                Model = "Yaris",
+                Mileage = 45000,
+                ReleasedYear = 2018,
+                LastRevisionYear = 2020,
+                IsTransmissionAutomatic = true
+
+            };
+
+            var jsonBody = JsonConvert.SerializeObject(car);
+            var body = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            //Act
+            var result = await httpClient.PostAsync("/api/cars", body);
+
+
+            //Assert
+            Assert.AreEqual(true, result.IsSuccessStatusCode);
+            Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
+        }
+
+        [Test]
+        public async Task given_a_car_with_invalid_information_when_insert_then_return_400_status()
+        {
+            //Arrange
+            var car = new CarModel
+            {
+                Make = "Toyota",
+                Model = "Yaris",
+                Mileage = -45000,
+                ReleasedYear = 2018,
+                LastRevisionYear = 2020,
+                IsTransmissionAutomatic = true
+
+            };
+
+            var jsonBody = JsonConvert.SerializeObject(car);
+            var body = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            //Act
+            var result = await httpClient.PostAsync("/api/cars", body);
+
+            //Assert
+            Assert.AreEqual(false, result.IsSuccessStatusCode);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task given_a_car_with_inconsistent_information_when_insert_then_return_400_status()
+        {
+            //Arrange
+            var car = new CarModel
+            {
+                Make = "Toyota",
+                Model = "Yaris",
+                Mileage = 45000,
+                ReleasedYear = 2020,
+                LastRevisionYear = 2015,
+                IsTransmissionAutomatic = true
+
+            };
+
+            var jsonBody = JsonConvert.SerializeObject(car);
+            var body = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            //Act
+            var result = await httpClient.PostAsync("/api/cars", body);
+
+            //Assert
+            Console.WriteLine(result.ReasonPhrase);
+            Assert.AreEqual(false, result.IsSuccessStatusCode);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            httpClient.Dispose();
+            testApplicationFactory.Dispose();
+        }
+    }
+}
---
+++ IntegrationTests/InMemoryDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DriveMeShop.Entity;
+using Mongo2Go;
+using MongoDB.Driver;
+
+namespace IntegrationTests
+{
+    public class InMemoryDB: IDisposable
+    {
+        private readonly MongoDbRunner mongoDbRunner;
+        private MongoClient client;
+        private IMongoDatabase database;
+
+        private const string DATABASE_NAME = "test_db";
+        private const string CAR_COLLECTION_NAME = "cars";
+
+        public InMemoryDB()
+        {
+            mongoDbRunner = MongoDbRunner.Start(singleNodeReplSet: false);
+
+            client = new MongoClient(mongoDbRunner.ConnectionString);
+            database = client.GetDatabase(DATABASE_NAME);
+            database.CreateCollection(CAR_COLLECTION_NAME);
+
+        }
+
+        public IMongoCollection<Car> GetCarCollection()
+        {
+            return database.GetCollection<Car>(CAR_COLLECTION_NAME);
+
+        }
+
+        public void Dispose()
+        {
+            mongoDbRunner.Dispose();
+        }
+    }
+}
