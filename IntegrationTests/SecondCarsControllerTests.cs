2d9c7c83e9a169d27b8acbf00971ad5bf3b4be53
Mamadou Diop <m.abdoulayediop@useradgents.com>
2022-03-09 07:42:20 +0100

    writing unit tests

--- IntegrationTests/SecondCarsControllerTests.cs
+++ IntegrationTests/SecondCarsControllerTests.cs
@@ -1,10 +1,89 @@
-﻿using System;
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DriveMeShop.Model;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
 namespace IntegrationTests
 {
-    public class SecondCarsControllerTests
+    public partial class CarsControllerTests
     {
-        public SecondCarsControllerTests()
+       [Test]
+       public async Task given_a_valid_car_id_when_updating_last_revision_year_with_valid_value_then_return_204()
         {
+            //Arrange
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2021
+            };
+
+            var jsonBody = JsonConvert.SerializeObject(lastRevisionYearModel);
+            var body = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            //Act
+            var result = await httpClient.PatchAsync("api/cars/6168c06d89af83d580f6e01e", body);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task given_a_valid_car_id_when_updating_last_revision_year_with_invalid_value_then_return_400()
+        {
+            //Arrange
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2015
+            };
+
+            var jsonBody = JsonConvert.SerializeObject(lastRevisionYearModel);
+            var body = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            //Act
+            var result = await httpClient.PatchAsync("api/cars/6168c06d89af83d580f6e01e", body);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task given_an_invalid_car_id_when_updating_last_revision_with_any_value_then_return_400()
+        {
+            //Arrange
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2020
+            };
+
+            var jsonBody = JsonConvert.SerializeObject(lastRevisionYearModel);
+            var body = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            //Act
+            var result = await httpClient.PatchAsync("api/cars/6168c06d89af8380f6e01e", body);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task given_an_unknown_car_id_when_trying_to_update_last_revision_year_with_any_value_then_return_404()
+        {
+            //Arrange
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2021
+            };
+
+            var jsonBody = JsonConvert.SerializeObject(lastRevisionYearModel);
+            var body = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            //Act
+            var result = await httpClient.PatchAsync("api/cars/61f667667cb328c6005c8392", body);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, result.StatusCode);
         }
     }
 }
--- UnitTests/SecondCarsControllerTest.cs
+++ UnitTests/SecondCarsControllerTest.cs
@@ -1,1 +1,279 @@
-﻿
+﻿using System;
+using System.Threading.Tasks;
+using DriveMeShop.Controllers;
+using DriveMeShop.Entity;
+using DriveMeShop.Model;
+using DriveMeShop.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    partial class CarsControllerTest
+    {
+        [Test]
+        public async Task given_a_car_with_valid_id_when_updating_last_revision_year_with_valid_value_succeeded_then_return_204()
+        {
+            //Arrange
+
+            var mockCar = new Car
+            {
+                Id = "112TVTVTVT",
+                Make = "BMW",
+                Model = "X6",
+                ReleasedYear = 2020,
+                TransmissionMode = "MANUAL",
+                Mileage = 5000
+            };
+
+            var carId = "112TVTVTVT";
+
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2021
+            };
+
+            var mockCarRepository = new Mock<ICarRepository>();
+
+            mockCarRepository.Setup(repository => repository.GetCar(It.IsAny<string>()))
+                .Returns(mockCar);
+
+            mockCarRepository.Setup(repository => repository.UpdateCarLastRevisionYearAsync(It.IsAny<string>(),It.IsAny<int?>()))
+                .Returns(Task.FromResult(carId));
+
+            //Act
+
+            var controller = new CarsController(mockCarRepository.Object);
+            var result = await controller.PatchAsync(carId, lastRevisionYearModel);
+
+            //Assert
+            Assert.IsInstanceOf<NoContentResult>(result);
+        }
+
+        [Test]
+        public async Task given_a_car_with_valid_id_when_updating_last_revision_year_with_valid_value_failed_then_return_500()
+        {
+            //Arrange
+
+            var mockCar = new Car
+            {
+                Id = "112TVTVTVT",
+                Make = "BMW",
+                Model = "X6",
+                ReleasedYear = 2020,
+                TransmissionMode = "MANUAL",
+                Mileage = 5000
+            };
+
+            var carId = "112TVTVTVT";
+
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2021
+            };
+
+            var mockCarRepository = new Mock<ICarRepository>();
+
+            mockCarRepository.Setup(repository => repository.GetCar(It.IsAny<string>()))
+                .Returns(mockCar);
+
+            mockCarRepository.Setup(repository => repository.UpdateCarLastRevisionYearAsync(It.IsAny<string>(), It.IsAny<int?>()))
+                .Returns(Task.FromResult<string>(null));
+
+            //Act
+
+            var controller = new CarsController(mockCarRepository.Object);
+            var result = await controller.PatchAsync(carId, lastRevisionYearModel);
+
+            //Assert
+            Assert.AreEqual(StatusCodes.Status500InternalServerError, ((ObjectResult)result).StatusCode);
+
+        }
+
+        [Test]
+        public async Task given_a_car_with_valid_id_when_updating_last_revision_year_with_invalid_value_then_return_400()
+        {
+            //Arrange
+
+            var mockCar = new Car
+            {
+                Id = "112TVTVTVT",
+                Make = "BMW",
+                Model = "X6",
+                ReleasedYear = 2020,
+                TransmissionMode = "MANUAL",
+                Mileage = 5000
+            };
+
+            var carId = "112TVTVTVT";
+
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2019
+            };
+
+
+            var mockCarRepository = new Mock<ICarRepository>();
+
+            mockCarRepository.Setup(repository => repository.GetCar(It.IsAny<string>()))
+                .Returns(mockCar);
+
+            //Act
+
+            var controller = new CarsController(mockCarRepository.Object);
+            var result = await controller.PatchAsync(carId, lastRevisionYearModel);
+
+            //Assert
+
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+
+        [Test]
+        public async Task given_a_car_which_does_not_exist_when_updating_last_revision_year_with_any_value_then_return_404()
+        {
+            //Arrange
+            var carId = "112TVTVTVT";
+
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2019
+            };
+
+            Car mockCar = null;
+
+            var mockCarRepository = new Mock<ICarRepository>();
+
+            mockCarRepository.Setup(repository => repository.GetCar(It.IsAny<string>()))
+                .Returns(mockCar);
+
+            //Act
+
+            var controller = new CarsController(mockCarRepository.Object);
+            var result = await controller.PatchAsync(carId, lastRevisionYearModel);
+
+            //Assert
+
+            Assert.IsInstanceOf<NotFoundObjectResult>(result);
+        }
+
+        [Test]
+        public async Task given_a_car_with_valid_id_when_updating_last_revision_year_throws_exception_then_return_500()
+        {
+            //Arrange
+
+            var mockCar = new Car
+            {
+                Id = "112TVTVTVT",
+                Make = "BMW",
+                Model = "X6",
+                ReleasedYear = 2020,
+                TransmissionMode = "MANUAL",
+                Mileage = 5000
+            };
+
+            var carId = "112TVTVTVT";
+
+            var mockException = new Exception("failure during update");
+
+            var lastRevisionYearModel = new CarLastRevisionYearModel
+            {
+                LastRevisionYear = 2021
+            };
+
+            var mockCarRepository = new Mock<ICarRepository>();
+
+            mockCarRepository.Setup(repository => repository.GetCar(It.IsAny<string>()))
+                .Returns(mockCar);
+
+            mockCarRepository.Setup(repository => repository.UpdateCarLastRevisionYearAsync(It.IsAny<string>(), It.IsAny<int?>()))
+                .Throws(mockException);
+
+            //Act
+
+            var controller = new CarsController(mockCarRepository.Object);
+            var result = await controller.PatchAsync(carId, lastRevisionYearModel);
+
+            //Assert
+
+            Assert.AreEqual(StatusCodes.Status500InternalServerError, ((ObjectResult)result).StatusCode);
+        }
+
+        [Test]
+        public async Task given_a_car_id_when_deletion_of_a_car_with_that_id_is_successful_then_return_204()
+        {
+            //Arrange
+
+            var carId = "173829UDJDJ";
+            var mockCar = new Car();
+
+            var mockCarRepository = new Mock<ICarRepository>();
+
+            mockCarRepository.Setup(repository => repository.GetCar(It.IsAny<string>()))
+                .Returns(mockCar);
+
+            mockCarRepository.Setup(repository => repository.DeleteCarAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(true));
+
+            //Act
+
+            var controller = new CarsController(mockCarRepository.Object);
+            var result = await controller.DeleteAsync(carId);
+
+            //Assert
+
+            Assert.IsInstanceOf<NoContentResult>(result);
+        }
+
+        [Test]
+        public async Task given_a_car_id_when_deletion_of_a_car_with_that_id_is_not_successful_then_return_500()
+        {
+            //Arrange
+
+            var carId = "173829UDJDJ";
+            var mockCar = new Car();
+
+            var mockCarRepository = new Mock<ICarRepository>();
+
+            mockCarRepository.Setup(repository => repository.GetCar(It.IsAny<string>()))
+                .Returns(mockCar);
+
+            mockCarRepository.Setup(repository => repository.DeleteCarAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(false));
+
+            //Act
+
+            var controller = new CarsController(mockCarRepository.Object);
+            var result = await controller.DeleteAsync(carId);
+
+            //Assert
+
+            Assert.AreEqual(StatusCodes.Status500InternalServerError, ((ObjectResult)result).StatusCode);
+
+        }
+
+        [Test]
+        public async Task given_a_car_id_when_a_car_with_that_id_is_not_found_then_return_404()
+        {
+            //Arrange
+            var carId = "33939";
+
+            Car mockCar = null;
+
+            var mockCarRepository = new Mock<ICarRepository>();
+
+            mockCarRepository.Setup(repository => repository.GetCar(It.IsAny<string>()))
+                .Returns(mockCar);
+
+            //Act
+
+            var controller = new CarsController(mockCarRepository.Object);
+            var result = await controller.DeleteAsync(carId);
+
+            //Assert
+
+            Assert.IsInstanceOf<NotFoundObjectResult>(result);
+        }
+    }
+}
